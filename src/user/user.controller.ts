c80cb9aee8c4c9ef6fd365d0b827d04bb5b5623e
NOZi <madiyar10002004@gmail.com>
2024-09-30 18:22:42 +0500

    Fix leak of password hash in profile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, UseGuards, Get, Request } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UserService } from './user.service';
+import { UserDto } from './user.dto';
 
 
 @Controller('user')
 export class UserController {
+  constructor(
+    private readonly userService: UserService
+  ) { }
+
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
+  async getProfile(@Request() req) {
+    const username = req.user.username;
+    let user = await this.userService.findByUsername(username);
+
+    user = { ...user, password: undefined };
+
+    return user;
   }
 }
---
+++ src/user/user.dto.ts
@@ -0,0 +1,6 @@
+export class UserDto {
+  id: number;
+  username: string;
+  totalGamesPlayed: number;
+  totalWins: number;
+}
